f851b494c0e3825d18785acc54ed195191411764
Robert Hollis <robert@rmbkenterprises.com>
2026-05-19 13:12:30 -0400

    fix production CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ["http://localhost:3001", "http://localhost:3002"],
+    origin: [
+      "https://www.clinicalsitehub.com",
+      "https://clinicalsitehub.com",
+    ],
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   });
 
